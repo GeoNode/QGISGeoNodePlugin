37730af689af5d94e1fcafbe65a1141356eadaf5
Samweli Mwakisambwe <samweli@kartoza.com>
2021-01-11 20:08:05 +0300

    Added tests on api_client logic (#44)
    added initial tests on api_client logic

--- test/conftest.py
+++ test/conftest.py
@@ -31,9 +31,107 @@ _geonode_flask_app.logger.removeHandler(flask.logging.default_handler)
 
 @_geonode_flask_app.route("/api/v2/layers/")
 def _mock_layer_list():
-    return {
+    style = {
+        "pk": 1,
+        "name": "test_style",
+        "workspace": "test",
+        "sld_title": "",
+        "sld_body": "",
+        "sld_version": "",
+        "sld_url": "http://testUrl"
+    }
+
+    layers = {
+        "links": {
+            "next": "",
+            "previous": ""
+        },
+        "page": 1,
         "page_size": 10,
-        "layers": []
+        "layers": [
+            {
+                "pk": 1,
+                "default_style": {},
+                "styles": [style]
+            },
+            {
+                "pk": 2,
+                "default_style": {},
+                "styles": [style]
+            }
+        ]
+    }
+
+    return layers
+
+
+@_geonode_flask_app.route("/api/v2/layers/<id>/")
+def _mock_layer_details(id):
+    id = int(id)
+    style = {
+                "pk": 1,
+                "name": "test_style",
+                "workspace": "test",
+                "sld_title": "",
+                "sld_body": "",
+                "sld_version": "",
+                "sld_url": "http://testUrl"
+    }
+    layers = [
+        {
+            "pk": 1,
+            "default_style": {},
+            "styles": [style]
+        },
+        {
+            "pk": 2,
+            "default_style": {},
+            "styles": [style]
+        }
+    ]
+
+    for layer in layers:
+        if id == layer["pk"]:
+            return {
+                "layer": layer
+            }
+
+    return {
+        "detail": "Not found."
+    }
+
+
+@_geonode_flask_app.route("/api/v2/layers/<id>/styles/")
+def _mock_layer_styles(id):
+    return {
+        "styles": [
+            {
+                "pk": 1,
+                "name": "test_style",
+                "workspace": "test",
+                "sld_title": "",
+                "sld_body": "",
+                "sld_version": "",
+                "sld_url": "http://testUrl"
+            }
+        ]
+    }
+
+
+@_geonode_flask_app.route("/api/v2/maps/")
+def _mock_map_list():
+    return {
+        "links": {
+            "next": "",
+            "previous": ""
+        },
+        "page": 1,
+        "page_size": 10,
+        "maps": [
+            {
+                "pk": "1"
+            }
+        ]
     }
 
 
@@ -45,15 +143,11 @@ def _spawn_geonode_server(port=9000):
 @pytest.fixture(scope="session")
 def mock_geonode_server():
     """Spawn a new GeoNode-like http server in a new process
-
     This fixture creates a mock GeoNode server to be used by tests. This mock server
     is a flask application that has fixed responses to the known-endpoints.
-
     The server is shutdown when a test run finishes
-
     Use this in tests that expect to communicate with a remote GeoNode server by adding
     `mock_geonode_server` as an extra test parameter
-
     """
 
     # TODO: allow configuring the port via an env variable
--- test/test_apiclient.py
+++ test/test_apiclient.py
@@ -35,4 +35,60 @@ def test_layer_list(qtbot, qgis_application, mock_geonode_server, page):
         client.get_layers(page=page)
     page_size = int(app.received_response["page_size"])
     print(f"layer ids: {[la['pk'] for la in app.received_response['layers']]}")
+
+    layers_size = len(app.received_response['layers'])
+    page_number = int(app.received_response['page'])
+
+    assert layers_size == 2
+    assert page_number == 1
     assert page_size == 10
+
+
+@pytest.mark.parametrize("id", [
+    pytest.param(1, id="1")
+])
+def test_layer_details(qtbot, qgis_application, mock_geonode_server, id):
+    app = ResponseCollector()
+    client = api_client.GeonodeClient(
+        # base_url="https://master.demo.geonode.org",
+        base_url = "http://localhost:9000",
+    )
+    client.layer_details_received.connect(app.collect_response)
+    with qtbot.waitSignal(client.layer_details_received, timeout=2*1000) as blocker:
+        client.get_layer_details(id=id)
+
+    pk = app.received_response["layer"]["pk"]
+    assert id == int(pk)
+
+
+@pytest.mark.parametrize("id", [
+    pytest.param(1, id="1")
+])
+def test_layer_styles(qtbot, qgis_application, mock_geonode_server, id):
+    app = ResponseCollector()
+    client = api_client.GeonodeClient(
+        base_url="http://localhost:9000",
+    )
+    client.layer_styles_received.connect(app.collect_response)
+    with qtbot.waitSignal(client.layer_styles_received, timeout=2*1000) as blocker:
+        client.get_layer_styles(id=id)
+        styles_size = len(app.received_response["styles"])
+    assert styles_size == 1
+
+
+@pytest.mark.parametrize("page", [
+    pytest.param(None, id="no explicit page")
+])
+def test_map_list(qtbot, qgis_application, mock_geonode_server, page):
+    app = ResponseCollector()
+    client = api_client.GeonodeClient(
+        base_url="http://localhost:9000",
+    )
+    client.map_list_received.connect(app.collect_response)
+    with qtbot.waitSignal(client.map_list_received, timeout=2*1000) as blocker:
+        client.get_maps(page=page)
+    page_size = int(app.received_response["page_size"])
+    print(f"maps ids: {[la['pk'] for la in app.received_response['maps']]}")
+
+    assert page_size == 10
+    assert int(app.received_response['maps'][0]['pk']) == 1
