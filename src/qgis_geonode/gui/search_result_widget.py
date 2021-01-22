04efcb5ea39dc9fad1869a56221156564f827f59
Ricardo Garcia Silva <ricardo.garcia.silva@likeno.pt>
2021-01-22 23:09:41 +0000

    Update with latest main branch

--- src/qgis_geonode/gui/search_result_widget.py
+++ src/qgis_geonode/gui/search_result_widget.py
@@ -3,6 +3,7 @@ import os
 from qgis.PyQt.QtWidgets import QWidget
 from qgis.PyQt.uic import loadUiType
 
+from ..api_client import BriefGeonodeResource
 from ..resources import *
 
 
@@ -12,8 +13,8 @@ WidgetUi, _ = loadUiType(
 
 
 class SearchResultWidget(QWidget, WidgetUi):
-    def __init__(self, name, description, parent=None):
+    def __init__(self, geonode_resource: BriefGeonodeResource, parent=None):
         super().__init__(parent)
         self.setupUi(self)
-        self.name_la.setText(name)
-        self.description_la.setText(description)
+        self.name_la.setText(geonode_resource.title)
+        self.description_la.setText(geonode_resource.abstract)
