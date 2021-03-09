5ca5f1ae9026d851c6c39f8f243bd1c322706dad
Samweli <samweli@kartoza.com>
2021-03-09 15:42:16 +0300

    store search buttons states and add tooltip for the resources types filter item

--- src/qgis_geonode/gui/geonode_source_select_provider.py
+++ src/qgis_geonode/gui/geonode_source_select_provider.py
@@ -332,6 +332,7 @@ class GeonodeDataSourceWidget(qgis.gui.QgsAbstractDataSourceWidget, WidgetUi):
         self.message_bar.clearWidgets()
         self.toggle_search_buttons()
 
+
     def clear_search_results(self):
         self.scroll_area.setWidget(QtWidgets.QWidget())
         self.resultsLabel.clear()
