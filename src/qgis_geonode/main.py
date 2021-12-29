9e074ecfe1c6b8fc7f2a5aafbee6d11152ff4ddb
Ricardo Garcia Silva <ricardo@kartoza.com>
2021-12-29 17:28:27 +0000

    Remove empty reference to the plugin in QGIS plugins menu (#196)

--- src/qgis_geonode/main.py
+++ src/qgis_geonode/main.py
@@ -129,15 +129,6 @@ class QgisGeoNode:
         return action
 
     def initGui(self):
-        """Create the menu entries and toolbar icons inside the QGIS GUI."""
-        icon_path = ":/plugins/Qgis_GeoNode/icon.png"
-        self.add_action(
-            icon_path,
-            text=self.tr(u""),
-            callback=self.run,
-            parent=self.iface.mainWindow(),
-        )
-
         self.iface.registerMapLayerConfigWidgetFactory(
             self.layer_properties_config_widget_factory
         )
